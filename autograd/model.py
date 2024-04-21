e328fbdb97a65b3c75ea77da1d0f9a83d02187cb
Frank Jansen <fphjansen@gmail.com>
2024-04-21 18:18:10 -0400

    autograd experiments

---
+++ autograd/model.py
@@ -0,0 +1,68 @@
+import torch                     # for all things PyTorch
+import torch.nn as nn            # for torch.nn.Module, the parent object for PyTorch models
+import torch.nn.functional as F  # for the activation function
+
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+BATCH_SIZE = 16
+DIM_IN = 1000
+HIDDEN_SIZE = 100
+DIM_OUT = 10
+
+class TinyModel(torch.nn.Module):
+
+    def __init__(self):
+        super(TinyModel, self).__init__()
+
+        self.layer1 = torch.nn.Linear(DIM_IN, HIDDEN_SIZE)
+        self.relu = torch.nn.ReLU()
+        self.layer2 = torch.nn.Linear(HIDDEN_SIZE, DIM_OUT)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.relu(x)
+        x = self.layer2(x)
+        return x
+
+some_input = torch.randn(BATCH_SIZE, DIM_IN, requires_grad=False)
+ideal_output = torch.randn(BATCH_SIZE, DIM_OUT, requires_grad=False)
+
+model = TinyModel()
+
+print(model.layer2.weight[0][0:10]) # just a small slice
+print(model.layer2.weight.grad)
+
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+
+prediction = model(some_input)
+
+loss = (ideal_output - prediction).pow(2).sum()
+print(loss)
+
+loss.backward()
+print(model.layer2.weight[0][0:10])
+print(model.layer2.weight.grad[0][0:10])
+
+optimizer.step()
+print(model.layer2.weight[0][0:10])
+print(model.layer2.weight.grad[0][0:10])
+
+print(model.layer2.weight.grad[0][0:10])
+
+for i in range(0, 5):
+    prediction = model(some_input)
+    loss = (ideal_output - prediction).pow(2).sum()
+    loss.backward()
+
+print(model.layer2.weight.grad[0][0:10])
+
+optimizer.zero_grad(set_to_none=False)
+
+print(model.layer2.weight.grad[0][0:10])
+
